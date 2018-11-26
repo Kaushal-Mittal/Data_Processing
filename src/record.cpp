f94b313a638f2f8421e4c29f42ec409874ee3bb1
Kaushal Mittal <kaushal.mittal@iiitb.org>
2018-11-27 01:32:33 +0530

    written data.h

---
+++ src/record.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include "record.h"
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
+
+Record::Record(){}
+
+Record::Record(string s, int n){
+    
+    vector<string> temp;
+    boost::split(temp, s, ";");
+    
+    for(size_t i = 0; i < temp.size(); i++)
+    {
+        cout << temp[i] << " ";    
+    
+    }
+    
+}
+Record::Record(Record &rec){
+
+}
+
+Record::~Record(){
+
+}
--- src/record.h
+++ src/record.h
@@ -1,13 +1,21 @@
 #ifndef RECORD_H_
 #define RECORD_H_
 
-#include "vector"
+#include <vector>
+#include <iostream>
+
+using namespace std;
 
 class Record{
     private:
         vector <int> feature;
+        vector <string> param;
     public:
+        Record();
+        Record(string s, int n);
+        Record(Record &rec);
+        ~Record();
         
-}
+};
 
 #endif
