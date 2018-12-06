d3841abdd18a771d8f38810e7753bc67f49ea424
Kaushal Mittal <kaushal.mittal@iiitb.org>
2018-12-06 10:09:08 +0530

    Covariance Matrix

--- src/task4.cpp
+++ src/task4.cpp
@@ -12,52 +12,49 @@
 using namespace std;
 
 task4::task4(){}
-
+////////////////////////////////////////////////////////////////////////////////////////
 task4::~task4(){}
-
+////////////////////////////////////////////////////////////////////////////////////////
+bool func(pair<int, double> p1, pair<int, double> p2){ // Custom Comparator to sort variances
+    return p1.second < p2.second ;
+}
+////////////////////////////////////////////////////////////////////////////////////////
 double  task4::generate_covariance(vector<double> v1, vector<double> v2){
 
-    double average1 = accumulate( v1.begin(), v1.end(), 0.0)/v1.size();
-    double average2 = accumulate( v2.begin(), v2.end(), 0.0)/v2.size();
+    double average1 = 0;
+    double average2 = 0;
+
+    average1 = accumulate( v1.begin(), v1.end(), 0.0)/v1.size();
+    average2 = accumulate( v2.begin(), v2.end(), 0.0)/v2.size();
 
     double covariance;
     
     // finding variance
     double temp = 0;
     for(int i= 0; i< v1.size(); i++) {
-        temp = ((v1[i] - average1)*(v2[i] - average2));
+        temp += ((v1[i] - average1)*(v2[i] - average2));
     }
-        covariance = temp/(v1.size()-1);
+    covariance = temp/(v1.size()-1);
 
     return covariance;
 }
-
-bool func(pair<int, double> p1, pair<int, double> p2){
-    return p1.second < p2.second ;
-}
-
+////////////////////////////////////////////////////////////////////////////////////////
 vector<vector<double> > task4::generate_covariance_matrix(Data dataobj){
-// void task4::generate_covariance_matrix(Data dataobj){
 
     int n = dataobj.getSize();
-
-    // typedef pair<int, double> pair<int, double>;
     vector<pair<int, double> > variances;
-
     
+//Finding Variance for each column 
     for(size_t i = 0; i < dataobj.getFeatureSize(); i++){
         int index = i;
         double variance;
         vector<double> v = dataobj.get_column(index);
-        // for(size_t j = 0; j < dataobj.getSize(); j++){   
-        //     v.push_back(dataobj.getData()[j].getFeatureVector()[i]);
-        // }-std=c++11
         variance = generate_covariance(v, v);
         variances.push_back(pair<int, double>(index, variance));
         v.clear();
     }
 
-// /////////////////////////////////////////////////////////
+// Normalisation
 
     int max = variances[0].second;
     int min = variances[0].second;
@@ -73,12 +70,12 @@ vector<vector<double> > task4::generate_covariance_matrix(Data dataobj){
     for(int i =0;i<variances.size();i++){
         variances[i].second = (variances[i].second-min)/(max-min);
     }
-// /////////////////////////////////////////////////////////
 
-
+//Sort using custom comparator func
     sort(variances.begin(), variances.end(), func);
-// [](pair<int, double> p1, pair<int, double> p2){return p1.second < p2.second ;}
 
+
+//Permutation [(old index, new index),()]
     vector<pair<int, int> > permutation;
     for(size_t i = 0; i < variances.size(); i++){
         permutation.push_back(pair<int, int>(i, variances[i].first));
@@ -88,21 +85,16 @@ vector<vector<double> > task4::generate_covariance_matrix(Data dataobj){
     vector<double> t(n, 0);
     vector<vector<double> > covar_matrix(n, t);
     
-    // cout << permutation.size() << endl;
-    
+//Generating Covariance Matrix
     for(int i = 0; i < permutation.size(); i++){
-        // vector<double> temp1;
-        // cout<<endl << i<< "---" ;
         for(int j = 0; j < permutation.size(); j++){
-            
-            double temp2;
 
+            double temp2;
             // vector<double> c1 = dataobj.get_column(permutation[i].second);
             // vector<double> c2 = dataobj.get_column(permutation[j].second);
-
             temp2 = generate_covariance(dataobj.get_column(permutation[i].second),  dataobj.get_column(permutation[j].second));
-            // cout << temp2 << " ";
             covar_matrix[i][j] = (temp2);
+
         }
     }
 
